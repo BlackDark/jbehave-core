e1c050b9fe6597654914a647f5048375f177744c
Eduard Marbach <mail@eduard-marbach.de>
2017-05-17 23:03:33 +0200

    Remove unused fields

--- jbehave-core/src/main/java/org/jbehave/core/model/Template.java
+++ jbehave-core/src/main/java/org/jbehave/core/model/Template.java
@@ -1,19 +1,11 @@
 package org.jbehave.core.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Template {
 
-    public static final Template EMPTY = new Template();
-
     private final String title;
     private final List<String> steps;
-
-    public Template() {
-        title = "";
-        steps = new ArrayList<String>();
-    }
 
     public Template(String title, List<String> steps) {
         this.title = title;
@@ -27,6 +19,5 @@ public class Template {
     public List<String> getSteps() {
         return steps;
     }
-
 
 }
